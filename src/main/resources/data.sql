d00a2d838f8b14a59cf6f4b8da8750274a297eb5
Seu Nome <seuemail@dominio.com>
2026-02-18 21:06:20 -0300

    Adicionando usuário admin inicial via data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO users (id, email, password, name, role, active) VALUES ('550e8400-e29b-41d4-a716-446655440000', 'admin@fitness.com', 'admin', 'Administrador', 'ADMIN', true) ON CONFLICT DO NOTHING;
