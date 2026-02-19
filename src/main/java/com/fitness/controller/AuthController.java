e6d040ac5c8eb1f8310c94cd6d6f5e94bcd89b8a
Seu Nome <seuemail@dominio.com>
2026-02-19 00:14:21 -0300

    Adicionando @CrossOrigin explícito para evitar 404/OPTIONS error

--- src/main/java/com/fitness/controller/AuthController.java
+++ src/main/java/com/fitness/controller/AuthController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@org.springframework.web.bind.annotation.CrossOrigin(origins = "*")
 public class AuthController {
 
     private final UserRepository userRepository;
