d28280c59894855c04fb493a672d65ddf5436e11
Seu Nome <seuemail@dominio.com>
2026-02-20 12:10:10 -0300

    chore: force trigger Railway deploy after fixing compilation

--- src/main/java/com/fitness/controller/StudentRelationsController.java
+++ src/main/java/com/fitness/controller/StudentRelationsController.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+// Force trigger deploy
 @RestController
 @RequestMapping("/api/student")
 @CrossOrigin(origins = "*")
